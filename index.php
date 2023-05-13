32d85222f6638acb598584efd4874d9f052a533e
Nelson John Montezo 1 <nj.montezo12@gmail.com>
2023-05-13 14:15:30 +0800

    my 2nd attempt commit

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
 
+// Parental Class
 abstract class Database
 {
     protected $conn;
     protected $db = "abstract";
 
-    public function __construct($conn)
+    public function __construct()
     {
         $this->conn = new mysqli("localhost", "root", "");
-        $this->conn->query("CREATE DATABASE IF NOT EXISTS $this->db");
-        $this->conn = new mysqli("localhost", "root", "", $this->db);
     }
 
-    abstract public function insert() : string;
+    abstract public function db() : string;
 }
 
-class Brand extends Database
+// Child Classes
+class Data extends Database
 {
-    public function insert() : string
+    public function db() : string
     {
-        return "This is original quality! I'm a $this->conn";
+       $data = "CREATE DATABASE IF NOT EXISTS $this->db";
+        return $this->conn->query($data);
     }
+
+    public function 
 }
